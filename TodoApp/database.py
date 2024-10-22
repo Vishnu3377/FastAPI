0e2af59df2766120d339ef7afd878ceedbb4f0f9
Vishnu3377 <vishnuvr3377@gmail.com>
2024-10-22 13:21:31 +0530

    Update database.py

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
